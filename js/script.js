8a934473384187cbca88480720e5c8598a1a1f84
francescovalenti <francescovalenti83@outlook.it>
2025-02-15 15:29:51 +0100

    feat:3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // inseriamo prima quanti km deve fare l'utente//
 // inseriamo quanti anni ha l'utente//
 
-let km= prompt ("Quanti km devi fare per raggingere la destinazione finale?")
+let km= prompt ("Quanti km devi fare per raggiungere la destinazione finale?")
 let age= prompt ("Quanti anni hai?")
 console.log(km, age);
+
+//inizio preparazione dei calcoli per gli sconti in base all'eta//
 
 let prezzobase= * 0.21;
 let sconto= * 0;
