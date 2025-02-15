00518b5de9099e52ff2dc2f16c71648c4a7b44c0
francescovalenti <francescovalenti83@outlook.it>
2025-02-15 15:24:50 +0100

    feat:1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// inseriamo prima quanti km deve fare l'utente//
+// inseriamo quanti anni ha l'utente//
+
+let km= prompt ("Quanti km devi fare per raggingere la destinazione finale?")
+let age= prompt ("Quanti anni hai?")
+console.log(km, age);
